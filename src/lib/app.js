c46fc264076d15298758e48a78219c73e9f0722a
Jered <jered.mccullough@ampush.com>
2017-09-13 14:56:13 -0700

    changes

--- src/lib/app.js
+++ src/lib/app.js
@@ -1,60 +1,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import ReactHighCharts from 'react-highcharts';
-const config = {
+import * as lines from '../../testing/mocks/graph-config';
 
-  title: {
-    text: 'Solar Employment Growth by Sector, 2010-2016'
-  },
-
-  subtitle: {
-    text: 'Source: thesolarfoundation.com'
-  },
-
-  yAxis: {
-    title: {
-      text: 'Number of Employees'
-    }
-  },
-  legend: {
-    layout: 'vertical',
-    align: 'right',
-    verticalAlign: 'middle'
-  },
-
-  plotOptions: {
-    series: {
-      pointStart: 2010
-    }
-  },
-
-  series: [{
-    name: 'Installation',
-    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-  }, {
-    name: 'Manufacturing',
-    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-  }, {
-    name: 'Sales & Distribution',
-    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-  }, {
-    name: 'Project Development',
-    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-  }, {
-    name: 'Other',
-    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-  }]
-
-};
-
+setTimeout(() => {
+  lines.heatGraph = _.cloneDeep(lines.line);
+  console.log(lines);
+}, 100);
+// const graphs = ;
 export default class App extends React.Component {
   componentDidMount() {
-    this.chart = this.refs.chart.getChart();
-    console.log(this);
+    console.log(this.refs);
   }
 
   render() {
-    return <ReactHighCharts ref = "chart" config = {config}></ReactHighCharts>;
+    return (
+      <div>
+        {_.map(lines, (config, key) =>
+          <ReactHighCharts key = {key} ref = {key} config = {config}></ReactHighCharts>)}
+      </div>
+    );
   }
 }
 ReactDom.render(<App></App>, document.querySelector('#main'));
